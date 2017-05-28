23af386d7599b2dee889c9ad40c394429eac313e
airwa1kin7 <derrick@klabouch.com>
2017-05-28 16:42:47 -0400

    v0.0.2
    Added chmod to change permissions after moving the script from the AOSiP-Helper-Bot folder to the build folder.

--- update_AHS.sh
+++ update_AHS.sh
@@ -1,6 +1,6 @@
 PATH=~/bin:$PATH
 echo -e '\e[91mThis build still contains test code. Not for daily use. Devs Only.\e[0m'
-echo -e '\e[104mStarting AOSiP Helper Bot Updater (v0.0.1) Press any key to clear the terminal and continue\e[0m'
+echo -e '\e[104mStarting AOSiP Helper Bot Updater (v0.0.2) Press any key to clear the terminal and continue\e[0m'
 	read -n 1 -s
 	clear
 	echo -e '\e[104mThis will update the helper script. Press any key update.\e[0m'
@@ -9,10 +9,11 @@ echo -e '\e[104mStarting AOSiP Helper Bot Updater (v0.0.1) Press any key to clea
 		echo -e '\e[96mHelper Bot just changed to Bot folder for updating the script\e[0m'
 		sleep 1
 		git pull
-		echo -e '\e[96mHelper Bot pulled git update\e[0m'
+		echo -e '\e[96mHelper Bot pulled git update. See above for changes and files pulled\e[0m'
 		sleep 1
 		cd $OLDPWD
 		mv AOSiP-Helper-Bot/aosip.sh $PWD
-		echo -e '\e[96mScript Updated. Press any key to quit.\e[0m'
+		chmod 777 aosip.sh
+		echo -e '\e[96mHelper Script Updated. Press any key to quit.\e[0m'
 		read -n 1 -s
 	exit
