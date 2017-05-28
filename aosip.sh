2a5f592d7635212d605580c2b6fe248906d02c94
airwa1kin7 <derrick@klabouch.com>
2017-05-28 17:42:35 -0400

    v0.9.3
    Added an automatic mode that syncs, lunch and builds the rom for you.

--- aosip.sh
+++ aosip.sh
@@ -3,13 +3,13 @@ echo -e '\e[32mThis version uses SLEEP to allow for easy reading. 1 second delay
 echo -e '\e[104mStarting AOSiP Helper. Press any key to clear the terminal and continue\e[0m'
 read -n 1 -s
 clear
-echo -e '\e[104mBuild and Sync Script v0.9.2 for AOSiP\e[0m'
+echo -e '\e[104mBuild and Sync Script v0.9.3 for AOSiP\e[0m'
 echo -e '\e[91mThis build still contains test code. Not for daily use. Devs Only.\e[0m'
 PATH=~/bin:$PATH
 echo -e '\e[96mHelper Bot just changed BIN Path to current folder to allow for Repo commands\e[0m'
 echo -e '\e[32mOffering choices to either Sync, Build or Quit\e[0m'
 PS3='Please enter your choice: '
-options=("Sync Repo" "Build AOSiP" "Dev Test" "Initialize AOSiP repo" "Quit")
+options=("Sync Repo" "Build AOSiP" "Auto Sync and Build" "Dev Test" "Initialize AOSiP repo" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -32,6 +32,27 @@ do
             echo -e '\e[32mStarting build\e[0m'
             sleep 1
             time mka kronic
+            
+            ;;
+        "Auto Sync and Build")
+            echo -e '\e[104mStarting Full Auto Mode\e[0m'
+	    echo -e '\e[91mYou will now be asked for your device name. Enter it when prompted. (Example angler)\e[0m'
+	    sleep 1
+            # devicename="USER INPUT"
+		read -p "Enter your device code name: " devicename
+		echo -e '\e[32mYou are going to build for $devicename\e[0m'
+		sleep 1
+	    echo -e '\e[32mStarting sync without using sync.sh\e[0m'
+            sleep 1
+			repo sync -c -f -j8 --force-sync --no-clone-bundle --no-tags
+			echo -e '\e[32mSync finished\e[0m'
+			sleep 1
+		echo -e '\e[32mStarting to build\e[0m'
+		source build/envsetup.sh
+		lunch aosip_$devicename-userdebug
+		echo -e '\e[32mGot ingredients for making Lunch. Time to cook\e[0m'
+			sleep 1
+		time mka kronic
             
             ;;
         "Dev Test")
