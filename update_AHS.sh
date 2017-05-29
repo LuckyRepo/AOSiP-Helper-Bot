e87bec5193c3d291077d7076fad320d309a97f29
airwa1kin7 <derrick@klabouch.com>
2017-05-28 20:19:34 -0400

    v0.0.2
    Added error handling if script is already up to date with github.

--- update_AHS.sh
+++ update_AHS.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 PATH=~/bin:$PATH
 echo -e '\e[91mThis build still contains test code. Not for daily use. Devs Only.\e[0m'
-echo -e '\e[104mStarting AOSiP Helper Bot Updater (v0.0.2) Press any key to clear the terminal and continue\e[0m'
+echo -e '\e[104mStarting AOSiP Helper Bot Updater v0.0.2\e[0m'
+echo -e '\e[104mPress any key to clear the terminal and continue\e[0m'
 	read -n 1 -s
 	clear
 	echo -e '\e[104mThis will update the helper script. Press any key update.\e[0m'
@@ -9,12 +10,15 @@ echo -e '\e[104mStarting AOSiP Helper Bot Updater (v0.0.2) Press any key to clea
 		cd AOSiP-Helper-Bot
 		echo -e '\e[96mHelper Bot just changed to Bot folder for updating the script\e[0m'
 		sleep 1
-		git pull
-		echo -e '\e[96mHelper Bot pulled git update. See above for changes and files pulled\e[0m'
+		if git pull | grep -q 'Already up-to-date.'; then
+  		echo -e '\e[91mThere where no changes detected. You should have the latest script.\e[0m'
+  		echo -e '\e[96mHelp Bot Updater will now exit.\e[0m'
+  		exit
+		fi
+		echo -e '\e[96mHelper Bot pulled git update\e[0m'
 		sleep 1
 		cd $OLDPWD
 		mv AOSiP-Helper-Bot/aosip.sh $PWD
-		chmod 777 aosip.sh
-		echo -e '\e[96mHelper Script Updated. Press any key to quit.\e[0m'
+		echo -e '\e[96mScript Updated. Press any key to quit.\e[0m'
 		read -n 1 -s
 	exit
