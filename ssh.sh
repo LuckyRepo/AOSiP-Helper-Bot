961e701407fec216ddfe5927bd11ebafddcb058d
airwa1kin7 <derrick@klabouch.com>
2017-07-23 02:25:47 -0400

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,2 @@
 #/bin/bash
-  read -p "Enter your GitHub Account email: " gitmail
-	echo -e '\e[32mYour GitHub Email is' $gitmail '\e[32m\e[0m'
-  		ssh-keygen -t rsa -b 4096 -C "$gitmail"
+	ssh-keygen -t rsa -b 4096 -C "$gitmail"
