76ab880bba947e7b66afc7d2764b4eae6ebdd64e
airwa1kin7 <derrick@klabouch.com>
2017-07-23 02:23:30 -0400

    v0.0.3
    SSH Keys

--- start.sh
+++ start.sh
@@ -4,6 +4,12 @@
     echo -e '\e[91mCurrently built for Ubuntu 15. Any other version and your on your own.\e[0m'
     echo
     echo
+#AOSiP needs SSH keys.
+		echo "Y" | ~/build/aosip/ssh.sh
+		echo -e '\e[96mDownload and open "id_rsa.pub" from "~/.ssh" on your server.\e[0m'
+		echo -e '\e[96mEnter it into github under settings and SSH keys.\e[0m'
+		echo -e '\e[96mContinue here after you have done that.\e[0m'
+		pause	
 # Delete buildpath.txt to avoid errors
         rm -rf buildpath.txt
 # Assign the build path from AOSiP build folder
@@ -37,16 +43,22 @@
   chmod a+x ~/bin/repo
     echo -e '\e[96mInstalled Repo\e[0m'
   pause 1
+
 # Set up AOSiP Help Bot from Github
     echo ----------------------------------
     echo -e '\e[96mSetting up AOSiP Help Bot\e[0m'
   git clone https://github.com/airwa1kin7/AOSiP-Helper-Bot
   mv AOSiP-Helper-Bot/aosip.sh $PWD
+  mv AOSiP-Helper-Bot/ssh.sh $PWD
   mv AOSiP-Helper-Bot/update_AHS.sh $PWD
   mv AOSiP-Helper-Bot/build.sh /root
   chmod 777 aosip.sh
+  chmod 777 ssh.sh
   chmod 777 update_AHS.sh
   cd ~
   chmod 777 build.sh
     echo -e '\e[32mHelper Bot ready to go\e[0m'
     echo -e '\e[32mSLOBS installed to root directory. Type "./build.sh" to use it.\e[0m'
+    echo -e '\e[32mLaunching Slobs.\e[0m'
+    	cd ~
+	./build.sh
