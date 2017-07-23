076a49f28655ace3fc58cd09a5176cacaceb3164
airwa1kin7 <derrick@klabouch.com>
2017-07-22 23:33:57 -0400

    v0.0.5a
    Not exactly called optimizing. Removed an old developer pause.

--- update_AHS.sh
+++ update_AHS.sh
@@ -1,11 +1,10 @@
 #/bin/bash
-PATH=~/bin:$PATH
+# Set bin path to avoid issues	
+	PATH=~/bin:$PATH
+# Usual blah blah blah
 echo -e '\e[91mThis build still contains test code. Not for daily use. Devs Only.\e[0m'
-echo -e '\e[104mStarting AOSiP Helper Bot Updater v0.0.4\e[0m'
+echo -e '\e[104mStarting AOSiP Helper Bot Updater v0.0.5a\e[0m'
 echo -e '\e[104mPress any key to clear the terminal and continue\e[0m'
-	read -n 1 -s
-	clear
-	echo -e '\e[104mThis will update the helper script. Press any key update.\e[0m'
 	read -n 1 -s
 		cd AOSiP-Helper-Bot
 		echo -e '\e[96mHelper Bot just changed to Bot folder for updating the script\e[0m'
