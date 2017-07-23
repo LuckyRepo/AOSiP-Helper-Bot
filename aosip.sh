6263e51ff3d0e3688a83845186dc28fb6b67dd65
airwa1kin7 <derrick@klabouch.com>
2017-07-22 22:36:32 -0400

    v0.9.4c

--- aosip.sh
+++ aosip.sh
@@ -7,6 +7,7 @@ echo -e '\e[104mBuild and Sync Script v0.9.4 for AOSiP\e[0m'
 echo -e '\e[91mThis build still contains test code. Not for daily use. Devs and testers Only.\e[0m'
 PATH=~/bin:$PATH
 echo -e '\e[96mHelper Bot just changed BIN Path to current folder to allow for Repo commands\e[0m'
+# Create a main menu.
 echo -e '\e[32mOffering choices to either Sync, Build or Quit\e[0m'
 PS3='Please enter your choice: '
 options=("Sync Repo" "Build AOSiP" "Auto Sync and Build" "Initialize Repo" "Quit")
