19aa6e4ce1c767e2ed0b5810ecd6f69b72eb2f15
LuckyRepo <derrick@klabouch.com>
2017-08-01 00:16:35 -0400

    v1.1.0

--- update_AHS.sh
+++ update_AHS.sh
@@ -3,7 +3,7 @@
 	PATH=~/bin:$PATH
 # Usual blah blah blah
 echo -e '\e[91mThis build still contains test code. Not for daily use. Devs Only.\e[0m'
-echo -e '\e[104mStarting AOSiP Helper Bot Updater v1.0.0\e[0m'
+echo -e '\e[104mStarting AOSiP Helper Bot Updater v1.1.0\e[0m'
 		cd AOSiP-Helper-Bot
 		echo -e '\e[96mHelper Bot just changed to Bot folder for updating the script\e[0m'
 		sleep 1
@@ -36,6 +36,23 @@ echo -e '\e[104mStarting AOSiP Helper Bot Updater v1.0.0\e[0m'
 #Setup Script Move
 			if mv AOSiP-Helper-Bot/start.sh $PWD | grep -q "mv: cannot stat 'AOSiP-Helper-Bot/start.sh': No such file or directory"; then
   			echo -e '\e[91mSetup Script did not need to be updated.\e[0m'
+  			else
+  			echo -e '\e[91mSetup Script Updated.\e[0m'
+			fi
+#Solo Build Move
+			if mv AOSiP-Helper-Bot/solobuild.sh $PWD | grep -q "mv: cannot stat 'AOSiP-Helper-Bot/solobuild.sh': No such file or directory"; then
+  			echo -e '\e[91mSolo Build did not need to be updated.\e[0m'
+  			else
+  			echo -e '\e[91mSetup Script Updated.\e[0m'
+#Repo Sync Move
+			if mv AOSiP-Helper-Bot/syncrepo.sh $PWD | grep -q "mv: cannot stat 'AOSiP-Helper-Bot/syncrepo.sh': No such file or directory"; then
+  			echo -e '\e[91mRepo Sync did not need to be updated.\e[0m'
+  			else
+  			echo -e '\e[91mSetup Script Updated.\e[0m'
+			fi
+#Auto Build Move
+			if mv AOSiP-Helper-Bot/autobuild.sh $PWD | grep -q "mv: cannot stat 'AOSiP-Helper-Bot/autobuild.sh': No such file or directory"; then
+  			echo -e '\e[91mAuto Build did not need to be updated.\e[0m'
   			else
   			echo -e '\e[91mSetup Script Updated.\e[0m'
 			fi
